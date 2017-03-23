aba76f1cdc83a20858b7fa659216635b97576396
Allegra Timsit <allegra.timsit@mdycehs.net>
2017-03-23 15:08:40 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(900, 900);
+}
+
+function draw() {
+    stroke(7, 75, 186);
+    strokeWeight(10);
+    fill( 0, 0, 0, 0);
+    ellipse(150, 150, 160, 160);
+    
+    stroke(0, 0,0);
+    strokeWeight(10);
+    fill( 0, 0, 0, 0);
+    ellipse(325, 150, 160, 160);
+    
+    stroke(249, 2, 6);
+    strokeWeight(10);
+    fill( 0, 0, 0, 0);
+    ellipse(500, 150, 160, 160);
+    
+    stroke(294, 204, 2);
+    strokeWeight(10);
+    fill( 0, 0, 0, 0);
+    ellipse(240, 245, 160, 160);
+    
+    stroke(38, 175, 3);
+    strokeWeight(10);
+    fill( 0, 0, 0, 0);
+    ellipse(420, 245, 160, 160);
+}
